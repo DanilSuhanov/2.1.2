bda06086ee7de99d2b5f85137e0c19a2a6b5196d
Данил <81508158+DanilSuhanov@users.noreply.github.com>
2022-10-08 10:54:36 +0300

    08.10 - done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("Cat");
+        Cat cat2 = (Cat) applicationContext.getBean("Cat");
+
+        System.out.println("Синглтон: бин1 равен бин2 - " + bean.equals(bean2));
+        System.out.println("Прототип: бин1 равен бин2 - " + cat1.equals(cat2));
     }
 }
